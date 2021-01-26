341bb528a37ad6dd884ad15dcb7e7a601f80580c
evelyn <ore.c0430@gmail.com>
2021-01-26 14:10:06 +1100

    add App.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-console.log('I get called from hello index.js!')
-
-const App = () => {
-    return <div>Testhello</div>
-}
+import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
